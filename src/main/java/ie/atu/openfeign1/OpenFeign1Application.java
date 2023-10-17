da68fb399c4eb4433ca162778640eab9ad676bf6
CianMurray98 <G00358011@atu.ie>
2023-10-17 16:46:47 +0100

    first microservice created

--- src/main/java/ie/atu/openfeign1/OpenFeign1Application.java
+++ src/main/java/ie/atu/openfeign1/OpenFeign1Application.java
@@ -2,8 +2,10 @@ package ie.atu.openfeign1;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.openfeign.EnableFeignClients;
 
 @SpringBootApplication
+@EnableFeignClients
 public class OpenFeign1Application {
 
     public static void main(String[] args) {
