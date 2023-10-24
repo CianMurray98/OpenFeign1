3076ce449ef7fb540956a39651e6aaaf1a9fe0b9
CianMurray98 <G00358011@atu.ie>
2023-10-24 13:43:49 +0100

    created classes for pt2, couldn't get talendAPI working

--- src/main/java/ie/atu/openfeign1/RegistrationServiceClient.java
+++ src/main/java/ie/atu/openfeign1/RegistrationServiceClient.java
@@ -4,8 +4,9 @@ import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 
-@FeignClient(name = "confirmation-service", url = "http://localhost:8082")
+@FeignClient(name = "confirmation-service", url = "http://localhost:8081")
 public interface RegistrationServiceClient {
-    @PostMapping("/confirm")
+    @PostMapping("/confirm-and-register")
     String someDetails(@RequestBody UserDetails userDetails);
+
 }
